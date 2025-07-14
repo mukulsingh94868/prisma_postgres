eb48225a5f249847f8957610fd5cdfb25594a923
mukul65 <mukul.singh@zentekinfosoft.com>
2025-07-14 15:45:29 +0530

    initial commit

---
+++ server.js
@@ -0,0 +1,21 @@
+import express from 'express'
+import { PrismaClient } from '@prisma/client'
+
+const app = express()
+const prisma = new PrismaClient()
+
+app.use(express.json())
+
+app.get('/', (req, res) => {
+  res.send('Hello from Prisma + Express + PostgreSQL!')
+})
+
+app.get('/users', async (req, res) => {
+  const users = await prisma.user.findMany()
+  res.json(users)
+})
+
+const PORT = process.env.PORT || 3000
+app.listen(PORT, () => {
+  console.log(`Server running on http://localhost:${PORT}`)
+})
